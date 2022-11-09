f8ad5f02edd5d3acd3a0fc2b03615c4aa0700bfa
TheDarioAu <90063181+TheDarioAu@users.noreply.github.com>
2022-11-09 14:53:36 -0700

    added blacklist

--- utils/api/sort_data.js
+++ utils/api/sort_data.js
@@ -1,12 +1,25 @@
 import {getTrelloData} from './trello_data.js'
 
 const getSortedData = (setCards,filters) => {
+  const blacklist = ["format", "changelog"]
   const hasWord = (str, word) => 
     str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(/\s+/).includes(word);
+  const notBlacklisted = (str) => {
+    let notBlacklist = true
+    blacklist.map((blacklist_word) => {
+      if (hasWord(str,blacklist_word)) {
+        notBlacklist = false
+      }
+    })
+    return notBlacklist
+  }
+    
+
+
   getTrelloData().then(function(data){
     let cards = []
     data.cards.map((card) => {
-      if (hasWord(card.desc,"talent")){
+      if (hasWord(card.desc,"talent") && notBlacklisted(card.name) ){
         if (filters != null && filters.length > 0){
           let filterid = ""
           data.labels.map((label) => {
